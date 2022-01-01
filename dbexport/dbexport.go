48827fc0cbe407c875fa44eef063ad6e3fd53ab2
baihai <hai.bai@enflame-tech.com>
2022-01-01 21:14:52 +0800

    fake a dtu-op trace

--- dbexport/dbexport.go
+++ dbexport/dbexport.go
@@ -2,6 +2,7 @@ package dbexport
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"os"
 
@@ -11,13 +12,16 @@ import (
 
 type ExtractOpInfo func(rtinfo.OpActivity) (bool, string, string)
 
-type AddOneTrace func(idx int, name string, ctxID int, startTS, endTS, durTS,
-	startCy, endCy, durCy uint64, opId int, opName string, clusterID int)
+type AddOpTrace func(
+	idx, nodeID, devID, clusterID, ctxID int, name string,
+	startTS, endTS, durTS uint64,
+	startCy, endCy, durCy uint64,
+	opId int, opName string)
 
 type DbSession struct {
 	targetName string
 	finish     func()
-	addTrace   AddOneTrace
+	addOpTrace AddOpTrace
 	idx        int
 }
 
@@ -50,18 +54,18 @@ func NewDbSession(target string) (*DbSession, error) {
 		log.Fatal(err)
 	}
 	stmt, err := tx.Prepare(`insert into dtu_op(
-		idx, name,
-		context_id,
+		idx, node_id, device_id, cluster_id, context_id, name,
 		start_timestamp, end_timestamp, duration_timestamp,
 		start_cycle, end_cycle, duration_cycle,
 		op_id, op_name,
-		cluster_id) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`)
-
-	// layer_kind, kind, fusion_kind, description, device is none
-	// meta is there but not for now
-	// tid is 0:0:0:xxx
-	// vp_id is 0 ~ max-1
-
+		vp_id, module_id,
+		row_name, tid)
+		values(?, ?, ?, ?, ?, ?,
+			   ?, ?, ?,
+			   ?, ?, ?,
+			   ?, ?,
+			   ?, ?,
+			   ?, ?)`)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -69,14 +73,29 @@ func NewDbSession(target string) (*DbSession, error) {
 		tx.Commit()
 		db.Close()
 	}
-	addTrace := func(idx int, name string, ctxID int, startTS, endTS, durTS,
-		startCy, endCy, durCy uint64, opId int, opName string, clusterID int) {
-		stmt.Exec(idx, name, ctxID, startTS, endTS, durTS,
-			startCy, endCy, durCy, opId, opName, clusterID)
+	addOpTrace := func(
+		idx, nodeID, devID, clusterID, ctxID int, name string,
+		startTS, endTS, durTS uint64,
+		startCy, endCy, durCy uint64,
+		opId int, opName string) {
+
+		moduleID := 1
+		vpId := idx
+
+		stmt.Exec(
+			idx, nodeID, devID, clusterID, ctxID, name,
+			startTS, endTS, durTS,
+			startCy, endCy, durCy,
+			opId, opName,
+			// and the default
+			vpId, moduleID,
+			"DTU Op", fmt.Sprintf("%v:%v:%v:%v:DTU Op",
+				nodeID, devID, ctxID, clusterID,
+			))
 	}
 	return &DbSession{
-		finish:   finish,
-		addTrace: addTrace,
+		finish:     finish,
+		addOpTrace: addOpTrace,
 	}, nil
 }
 
@@ -92,6 +111,9 @@ func (dbs *DbSession) DumpToEventTrace(
 ) {
 	dtuOpCount := 0
 	convertToHostError := 0
+	const nodeID = 0
+	const deviceID = 0
+	const clusterID = -1
 	for _, act := range bundle {
 		///act.IsOpRefValid()
 		if okToShow, _, name := extractor(act); okToShow {
@@ -99,11 +121,12 @@ func (dbs *DbSession) DumpToEventTrace(
 			startHostTime, startOK := tm.MapToHosttime(act.StartCycle())
 			endHostTime, endOK := tm.MapToHosttime(act.EndCycle())
 			if startOK && endOK {
-				dbs.addTrace(dbs.idx, name, act.Start.Context,
+				dbs.addOpTrace(dbs.idx, nodeID, deviceID, clusterID, act.Start.Context, name,
 					startHostTime, endHostTime, endHostTime-startHostTime,
 					act.StartCycle(), act.EndCycle(), act.EndCycle()-act.StartCycle(),
-					act.GetOp().OpId, act.GetOp().OpName, act.DpfAct.Start.ClusterID,
+					act.GetOp().OpId, act.GetOp().OpName,
 				)
+				dbs.idx++
 			} else {
 				convertToHostError++
 			}
