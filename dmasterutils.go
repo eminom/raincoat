9538d3332748fb8bd3af651899c2f645193d6cf8
baihai <hai.bai@enflame-tech.com>
2022-02-09 13:01:06 +0800

    add a forced time overlapping check(panic if not expected)

--- dmasterutils.go
+++ dmasterutils.go
@@ -1,19 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
+	"git.enflame.cn/hai.bai/dmaster/codec"
 	"git.enflame.cn/hai.bai/dmaster/rtinfo/rtdata"
 )
 
 func dumpFullCycles(bundle []rtdata.OpActivity) {
 	var intvs rtdata.Interval
 	// Check intervals
+
+	// master-id to last cycle value
+	prevCycleEnds := make(map[int]uint64)
+
 	for _, act := range bundle {
 		intvs = append(intvs, []uint64{
 			act.StartCycle(),
 			act.EndCycle(),
 		})
+
+		combinedCtx := act.Start.MasterIdValue()<<codec.RTCONTEXT_BITCOUNT +
+			act.Start.Context
+		if lastEndCycle, ok := prevCycleEnds[combinedCtx]; ok {
+			if act.StartCycle() <= lastEndCycle {
+				panic(fmt.Errorf("error crossed for master-id:%v, context: %v",
+					act.Start.MasterIdValue(),
+					act.Start.Context,
+				))
+			}
+		}
+		// update the last one
+		prevCycleEnds[combinedCtx] = act.EndCycle()
 	}
 	// sort.Sort(intvs)
 	overlappedCount := 0
